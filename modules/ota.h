33d008107098035de0965af44f70ac045f0a855a
arendst <mhtarends@gmail.com>
2016-01-25 11:29:44 +0100

    Create ota.h

---
+++ modules/ota.h
@@ -0,0 +1,17 @@
+#ifndef _OTA_H_
+#define _OTA_H_
+
+#ifndef TDEBUG
+#define TDEBUG 0
+#endif
+
+/*
+* Only http is supported but a different port may be used.
+* The following urls are handled correctly:
+*   http://sidnas2:80/api/sensor1/user1.bin
+*   http://sidnas2/api/sensor1/user1.bin
+*   sidnas2/api/sensor1/user1.bin
+*/
+void ICACHE_FLASH_ATTR start_ota(char *url);
+
+#endif
