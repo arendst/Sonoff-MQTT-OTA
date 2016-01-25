01624ef51001ffcf0a06347900c268c3c00d7cc9
arendst <mhtarends@gmail.com>
2016-01-25 11:30:49 +0100

    Create rtc.h

---
+++ modules/rtc.h
@@ -0,0 +1,29 @@
+#ifndef _RTC_H_
+#define _RTC_H_
+
+#ifndef TDEBUG
+#define TDEBUG 0
+#endif
+
+#define NTP_SERVER1 "pool.ntp.org"
+#define NTP_SERVER2 "nl.pool.ntp.org"
+#define NTP_SERVER3 "0.nl.pool.ntp.org"
+
+typedef struct {
+  uint8_t Second;
+  uint8_t Minute;
+  uint8_t Hour;
+  uint8_t Wday;   // day of week, sunday is day 1
+  uint8_t Day;
+  uint8_t Month;
+  uint16_t Year;
+  unsigned long Valid;
+} TIME_T;
+
+extern TIME_T rtcTime;
+
+void rtc_init(uint8_t timezone);
+void rtc_timezone(uint8_t timezone);
+void rtc_second();
+
+#endif
